17c750e7ce152eadad59f830311baaf8073e0746
Mike Anderson <mandersonjr19@gmail.com>
2018-12-18 23:12:35 -0700

    add files

--- building_10_python_apps/07_wizard_battle_app/actors.py
+++ building_10_python_apps/07_wizard_battle_app/actors.py
@@ -1,12 +1,31 @@
-
+import random
 
 class Wizard:
     def __init__(self, name, level):
         self.name = name
         self.level = level
 
+    def attack(self, creature):
+        print("The wizard {} attacks {}!".format(self.name, creature.name))
+
+        my_roll = random.randint(1, 12) * self.level
+        creature_roll = random .randint(1, 12) * creature.level
+
+        print("You rolled {}...".format(my_roll))
+        print("{} rolled {}".format(creature.name, creature_roll))
+
+        if my_roll >= creature_roll:
+            print("The wizard has won the battle over {}!".format(creature.name))
+            return True
+        else:
+            print("The wizard has been DEFEATED!!!")
+            return False
+
 
 class Creatures:
     def __init__(self, name, level):
         self.name = name
         self.level = level
+
+    def __repr__(self):
+        return "Creature: {} of level {}".format(self.name, self.level)
--- building_10_python_apps/07_wizard_battle_app/program.py
+++ building_10_python_apps/07_wizard_battle_app/program.py
@@ -1,3 +1,6 @@
+import random
+import time
+
 from actors import Wizard, Creatures
 
 
@@ -30,17 +33,31 @@ def game_loop():
 
     while True:
 
+        active_creature = random.choice(creatures)
+        print("A {}  of level {} has appeared from a dark and foggy forest....".format(active_creature.name, active_creature.level))
+
         cmd = input("Do you [a]ttack, [r]unaway, or [l]ook around? ")
         cmd = cmd.lower()
         if cmd == 'a':
-            print('attack')
+            if hero.attack(active_creature):
+                creatures.remove(active_creature)
+            else:
+                print("The wizard runs and hides taking time to recover....")
+                time.sleep(5)
+                print("The Wizard returns healthy!")
+
         elif cmd == 'r':
             print('runaway')
         elif cmd == 'l':
-            print('look around')
+            print('The wizard {} takes in the surroundings and sees: '.format(hero.name))
+            for c in creatures:
+                print(" * A {} of level {}".format(
+                    c.name, c.level
+                ))
         else:
             print("OK, exiting game... bye!")
             break
+
 
 if __name__ == '__main__':
     main()
